b47b7a68df9facefe681565e47342da8420c3463
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-12-05 13:42:52 +0300

    Connect summarizer to Kafka

--- summarization/main.py
+++ summarization/main.py
@@ -1,13 +1,28 @@
-import json
-import summarize
+from json import loads
+from kafka import KafkaConsumer
+
+from summarize import summarize
 
 
 def main():
-    articleInfo = None
-    with open("sample.json", 'r') as f:
-        articleInfo = f.read()
+    topics = ['investing-events', 'instaforex-events']
+    consumer = KafkaConsumer(
+        *topics,
+        bootstrap_servers=['kafka:29092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='summarization_consumer',
+        value_deserializer=lambda x: loads(x.decode('utf-8'))
+    )
+    print("Uploading received data...")
+    for entry in consumer:
+        articleInfo = entry.value
+        print(articleInfo['link'])
+        articleInfo['summary'] = summarize(articleInfo['text'])
+        print(articleInfo['summary'])
 
-    print(summarize.summarize(json.loads(articleInfo)["text"]))
+
+        # print("Uploaded entry successfully! ")
 
 
 if __name__ == "__main__":
