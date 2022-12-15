c9e861e2af187c3dcd4271f4666cfe4ce5c30a45
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-12-15 23:38:07 +0300

    Implement Kafka Producer

---
+++ investing_grabber/src/KafkaProducer.py
@@ -0,0 +1,13 @@
+from json import dumps
+from kafka import KafkaProducer
+
+
+def uploadData(data):
+    producer = KafkaProducer(bootstrap_servers=['kafka:29092'],
+                             value_serializer=lambda x:
+                             dumps(x).encode('utf-8'))
+
+    for entry in data:
+        producer.send('investing-events', value=entry)
+
+    print("Uploaded data to Kafka topic.")
